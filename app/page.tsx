500e5c27cea854d1bc78f92accfbbe8be1952ae5
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2024-03-26 18:24:17 +0500

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { Volume2 } from "lucide-react";
 import { useState } from "react";
 import Image from "next/image";
+import { Short_Stack } from "next/font/google";
 
 interface Question {
   title: string;
@@ -14,6 +14,8 @@ declare global {
     webkitSpeechRecognition: any;
   }
 }
+
+const shortStack = Short_Stack({ weight: "400", subsets: ["latin"] });
 
 export default function Home() {
   const [questions, setQuestions] = useState<Question[]>([]);
@@ -136,7 +138,84 @@ export default function Home() {
         </div>
 
         <div className="bg-white h-full w-full rounded-2xl relative justify-center items-center">
-          Hello World
+          <div className={shortStack.className}>
+            <div className="flex flex-col  space-y-2 px-5">
+              <div className="relative items-center justify-center">
+                <Image
+                  src="/pink-rectangle.svg"
+                  alt="Pink Rectangle"
+                  width={400}
+                  height={400}
+                />
+                <h1 className="absolute inset-0 flex text-xl items-center justify-center text-white">
+                  Elements of Story Writing
+                </h1>
+              </div>
+              <p className="px-3">
+                Welcome to term 2 of Writers Club. Are you excited for this
+                amazing journey? Over the next week, we will be practicing the
+                different elements of story writing. Our aim is to make stories
+                more interesting and exciting.
+              </p>
+              <div className="relative">
+                <Image
+                  src="/blue-rectangle.svg"
+                  alt="Blue Rectangle"
+                  width={300}
+                  height={300}
+                  className="w-full"
+                />
+                <h1 className="absolute ml-7 mr-36 inset-0 flex text-xl items-center justify-center text-white">
+                  There are elements which make the foundation for story
+                  writing. An element is an essential part of something and
+                  every fiction story has the same key elements:
+                </h1>
+                <Image
+                  src="/student.svg"
+                  alt="Student"
+                  width={80}
+                  height={80}
+                  className="absolute top-5 right-10"
+                />
+              </div>
+              <div className="flex space-x-4">
+                <Image
+                  src="/cloud-1.svg"
+                  alt="Cloud 1"
+                  width={150}
+                  height={150}
+                />
+                <Image
+                  src="/cloud-2.svg"
+                  alt="Cloud 2"
+                  width={150}
+                  height={150}
+                />
+                <Image
+                  src="/cloud-3.svg"
+                  alt="Cloud 3"
+                  width={150}
+                  height={150}
+                />
+                <Image
+                  src="/cloud-4.svg"
+                  alt="Cloud 4"
+                  width={150}
+                  height={150}
+                />
+                <Image
+                  src="/cloud-5.svg"
+                  alt="Cloud 5"
+                  width={150}
+                  height={150}
+                />
+              </div>
+              <p className="px-3">
+                You must be familiar with some of these. If not, do not worry!
+                we will cover all these elements as we go along.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
